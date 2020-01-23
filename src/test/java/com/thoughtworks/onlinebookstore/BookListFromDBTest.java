cf139bfada792803bdb1c9f24f0ac66858fe61a1
prajkta <prajktasraut16@gmail.com>
2020-01-23 18:15:47 +0530

    Commented the code because getBookById Api functioanlity is removed from the project

--- src/test/java/com/thoughtworks/onlinebookstore/BookListFromDBTest.java
+++ src/test/java/com/thoughtworks/onlinebookstore/BookListFromDBTest.java
@@ -24,14 +24,14 @@ public class BookListFromDBTest extends BaseTest {
             int statusCode = response.getStatusCode();
             ResponseBody body = response.getBody();
             Object Object = new JSONParser().parse(body.prettyPrint());
-            Assert.assertEquals(statusCode,200);
+            Assert.assertEquals(200,statusCode);
         } catch (ParseException e) {
             e.printStackTrace();
         }
     }
 
-    @Test
-    public void givenGetBookApi_WhenHitApi_ShouldReturnThatBookDetails() {
+    /*@Test
+    public void givenGetBookApi_WhenHitApi_ShouldReturnParticularBookDetails() {
         try {
             Response response=RestAssured.given()
                     .contentType(ContentType.JSON)
@@ -43,7 +43,7 @@ public class BookListFromDBTest extends BaseTest {
             int statusCode = response.getStatusCode();
             ResponseBody body = response.getBody();
             Object Object = new JSONParser().parse(body.prettyPrint());
-            Assert.assertEquals(statusCode,200);
+            Assert.assertEquals(200,statusCode);
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -62,7 +62,79 @@ public class BookListFromDBTest extends BaseTest {
             int statusCode = response.getStatusCode();
             ResponseBody body = response.getBody();
             Object Object = new JSONParser().parse(body.prettyPrint());
-            Assert.assertEquals(statusCode,500);
+            Assert.assertEquals(500,statusCode);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+*/
+    @Test
+    public void givenGetBookByNameApi_WhenHitApiWithProperBookName_ShouldReturnBookDetails() {
+        try {
+            Response response=RestAssured.given()
+                    .contentType(ContentType.JSON)
+                    .accept(ContentType.JSON)
+                    .when()
+                    .pathParam("bookName","Angel")
+                    .get("/bookByName/{bookName}");
+            int statusCode = response.getStatusCode();
+            ResponseBody body = response.getBody();
+            Object Object = new JSONParser().parse(body.prettyPrint());
+            Assert.assertEquals(200,statusCode);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenGetBookByNameApi_WhenHitApiWithWrongBookName_ShouldReturnNotFoundMessage() {
+        try {
+            Response response=RestAssured.given()
+                    .contentType(ContentType.JSON)
+                    .accept(ContentType.JSON)
+                    .when()
+                    .pathParam("bookName","abcd")
+                    .get("/bookByName/{bookName}");
+            int statusCode = response.getStatusCode();
+            ResponseBody body = response.getBody();
+            Object Object = new JSONParser().parse(body.prettyPrint());
+            Assert.assertEquals(404,statusCode);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenGetBookByNameApi_WhenHitApiWithoutBookName_ShouldReturnNotFoundMessage() {
+        try {
+            Response response=RestAssured.given()
+                    .contentType(ContentType.JSON)
+                    .accept(ContentType.JSON)
+                    .when()
+                    .pathParam("bookName","")
+                    .get("/bookByName/{bookName}");
+            int statusCode = response.getStatusCode();
+            ResponseBody body = response.getBody();
+            Object Object = new JSONParser().parse(body.prettyPrint());
+            Assert.assertEquals(404,statusCode);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenGetBookByNameApi_WhenHitApiWithNumberAsBookName_ShouldReturnNotFoundMessage() {
+        try {
+            Response response=RestAssured.given()
+                    .contentType(ContentType.JSON)
+                    .accept(ContentType.JSON)
+                    .when()
+                    .pathParam("bookName","1234")
+                    .get("/bookByName/{bookName}");
+            int statusCode = response.getStatusCode();
+            ResponseBody body = response.getBody();
+            Object Object = new JSONParser().parse(body.prettyPrint());
+            Assert.assertEquals(404,statusCode);
         } catch (ParseException e) {
             e.printStackTrace();
         }
